83123e99ed4d85d33acf3fabf5a9cb772e5fe6ba
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-02 18:01:27 +0900

    개발 중

--- YoutubeTagMaker.py
+++ YoutubeTagMaker.py
@@ -10,15 +10,16 @@ headers = {'User-Agent': 'Mozilla/5.0'}                             # 봇 방지
 # 정해진 유튜브 채널의 동영상 탭 내 정해진 날짜 시간을 만족하는 동영상 제목과 Iframe Tag를 생성하는 프로그램
 
 res = requests.get(url[0], headers=headers)
+
 if res.status_code == 200:
     # 응답 html코드를 text로 변환
     html = res.text
-    
+
     # 응답받은 html코드를 BeautifulSoup에 사용하기 위하여 인스턴스 지정
     soup = BeautifulSoup(html, 'html.parser')
 
     # tbody 에 필요한 게시글 목록이 있어 해당 영역 가져오기 처리
-    youtubebody = soup.select('.style-scope ytd-section-list-renderer')
+    youtubebody = soup.select('#primary > ytd-section-list-renderer')
 
     print(len(youtubebody))
 else:
