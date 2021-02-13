eef1c4a4f7e9ec7c73b1ad49cf36bbf50e233901
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-13 12:16:10 +0900

    유튜브 크롤러 개발 완료

--- youtube_crawler.py
+++ youtube_crawler.py
@@ -1,3 +1,4 @@
+# 시스템 함수 sys를 import 한다.
 import sys
 # selenium에서 webdriver를 사용할 수 있게 webdriver를 import 한다.
 from selenium import webdriver
@@ -9,6 +10,10 @@ from datetime import datetime, timedelta
 from yt_iframe import yt 
 # 월, 년 단위 계산 위해서 relativedelta를 import 한다.
 from dateutil.relativedelta import relativedelta
+# 숫자만 추출하기 위한 re를 import 한다.
+import re
+# 파일 존재 여부 확인 위한 os를 import 한다.
+import os
 
 # <p>채널명 [1번만 표시]</p><p>제목</p><p><a>URL</a></p><p><iframe></p> 로 작성
 
@@ -25,9 +30,9 @@ from dateutil.relativedelta import relativedelta
 # 고몽 https://www.youtube.com/user/rladndgussla/videos
 # 천재이승국 GeniusSKLee https://www.youtube.com/c/GeniusSKLee/videos
 # 은빛유니콘 https://www.youtube.com/channel/UCPq0i_bZeqVoVYm_u8qimDA/videos
-# 엔스Ens https://www.youtube.com/channel/UC_Aly3X5CdojHdRDGmKi1ow
+# 엔스Ens https://www.youtube.com/channel/UC_Aly3X5CdojHdRDGmKi1ow/videos
 # 가전주부 GJJB https://www.youtube.com/channel/UC5aNQ65ADb02zEJxzb_zmYQ/videos
-# 멋진기영TV https://www.youtube.com/channel/UCl8OVxF0iHY3uFEgXz9w00g
+# 멋진기영TV https://www.youtube.com/user/dlrldud1111/videos
 # B Man 삐맨 https://www.youtube.com/c/BMan%EC%82%90%EB%A7%A8/videos
 # 빨강도깨비 https://www.youtube.com/c/%EB%B9%A8%EA%B0%95%EB%8F%84%EA%B9%A8%EB%B9%84dokkebi/videos
 # 발없는새 https://www.youtube.com/user/nofeetbird/videos
@@ -38,7 +43,13 @@ from dateutil.relativedelta import relativedelta
 # LikeRing https://www.youtube.com/channel/UC7A1QdDXcu3zu_KS8DddL1A/videos
 # 사람사는세상노무현재단 https://www.youtube.com/c/%EC%82%AC%EB%9E%8C%EC%82%AC%EB%8A%94%EC%84%B8%EC%83%81%EB%85%B8%EB%AC%B4%ED%98%84%EC%9E%AC%EB%8B%A8/videos
 
-urllist = ['https://www.youtube.com/c/14FMBC/videos',
+# urllist = [
+#           'https://www.youtube.com/channel/UC_Aly3X5CdojHdRDGmKi1ow'
+#           ]
+
+# 추출할 유튜브 채널의 동영상 탭
+urllist = [
+           'https://www.youtube.com/c/14FMBC/videos',
            'https://www.youtube.com/c/inanutshell/videos',
            'https://www.youtube.com/teded/videos',
            'https://www.youtube.com/c/Vox/videos',
@@ -51,9 +62,9 @@ urllist = ['https://www.youtube.com/c/14FMBC/videos',
            'https://www.youtube.com/user/rladndgussla/videos',
            'https://www.youtube.com/c/GeniusSKLee/videos',
            'https://www.youtube.com/channel/UCPq0i_bZeqVoVYm_u8qimDA/videos',
-           'https://www.youtube.com/channel/UC_Aly3X5CdojHdRDGmKi1ow',
+           'https://www.youtube.com/channel/UC_Aly3X5CdojHdRDGmKi1ow/videos',
            'https://www.youtube.com/channel/UC5aNQ65ADb02zEJxzb_zmYQ/videos',
-           'https://www.youtube.com/channel/UCl8OVxF0iHY3uFEgXz9w00g',
+           'https://www.youtube.com/user/dlrldud1111/videos',
            'https://www.youtube.com/c/BMan%EC%82%90%EB%A7%A8/videos',
            'https://www.youtube.com/c/%EB%B9%A8%EA%B0%95%EB%8F%84%EA%B9%A8%EB%B9%84dokkebi/videos',
            'https://www.youtube.com/user/nofeetbird/videos',
@@ -62,7 +73,8 @@ urllist = ['https://www.youtube.com/c/14FMBC/videos',
            'https://www.youtube.com/channel/UCfcgDLazgMa1L92Kl3r9ZAA/videos',
            'https://www.youtube.com/c/%ED%95%9C%EC%83%81%EA%B8%B0HanSangKi/videos',
            'https://www.youtube.com/channel/UC7A1QdDXcu3zu_KS8DddL1A/videos',
-           'https://www.youtube.com/c/%EC%82%AC%EB%9E%8C%EC%82%AC%EB%8A%94%EC%84%B8%EC%83%81%EB%85%B8%EB%AC%B4%ED%98%84%EC%9E%AC%EB%8B%A8/videos']
+           'https://www.youtube.com/c/%EC%82%AC%EB%9E%8C%EC%82%AC%EB%8A%94%EC%84%B8%EC%83%81%EB%85%B8%EB%AC%B4%ED%98%84%EC%9E%AC%EB%8B%A8/videos'
+          ]
 
 # 전일 오전 7시
 yesterday = datetime.today() - timedelta(days=1)
@@ -73,8 +85,11 @@ todate = datetime(datetime.today().year, datetime.today().month, datetime.today(
 
 # 채널 리스트를 차례대로 불러옵니다.
 for u in range(0, len(urllist)):
+    # 로그를 없애는 설정
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
     # driver란 변수에 객체를 만들어 준다. chromedriver는 파이썬이 있는 경로에 두거나, 다른 경로에 두면 전체 경로명을 다 적어 줍니다.
-    driver = webdriver.Chrome(executable_path='chromedriver')
+    driver = webdriver.Chrome(executable_path='chromedriver', options=options)
 
     # 원하는 사이트의 url을 입력하여 사이트를 연다.
     driver.get(urllist[u])
@@ -117,83 +132,101 @@ for u in range(0, len(urllist)):
     # 분 전, 시간 전, 일 전, 주 전, 개월 전, 년 전
     time = [all_time[n].text for n in range(1,len(all_time),2)]
 
+    # iframe 태그 생성을 위해 폭과 높이를 설정
     width = '560' # (Optional)
     height = '315' # (Optional)
 
-    # 파일 변수 글로벌로 이동
+    # 파일명을 날짜로 이용하기 위해 글로벌로 이동
     nowDate = datetime.now()
-    # 파일 작성 시간이 길어져서 년월일로 파일명 생성
-    f = open(nowDate.strftime('%Y-%m-%d') + '_youtube.txt', mode='wt', encoding='utf-8')
 
-    # 파일에 1번만 저장하도록 수정 필요
+    if os.path.isfile(nowDate.strftime('%Y-%m-%d') + '_youtube.txt'):
+        # 파일이 존재할 경우 추가, 파일 작성 시간이 길어져서 년월일로 파일명 생성
+        f = open(nowDate.strftime('%Y-%m-%d') + '_youtube.txt', mode='at', encoding='utf-8')
+    else:
+        # 파일이 존재하지 않을 경우 생성, 파일 작성 시간이 길어져서 년월일로 파일명 생성
+        f = open(nowDate.strftime('%Y-%m-%d') + '_youtube.txt', mode='wt', encoding='utf-8')
+
+    # 채널명은 반복문 전 파일에 1번만 저장하도록 
     channelheader = "<p>" + channelname[0] + "</p>"
     channelheader += "\n"
+
+    print("#################################################################3")
+    print(channelname[0])
+    print("#################################################################3")
 
     fileContent = channelheader
 
     if (f is not None) and f.write(fileContent):
         print("fileContent write OK ")
 
-    fileContent = ""    # fileContent 초기화
-
+    # 화면에 로드된 동영상 갯수만큼
     for x in range(0, len(time)-1):
 
-        delta = ''
-
-        if '분 전' in time[x]:
-            # 분일 경우 작성 시간 확인
-            timenumber = time[x][0:time[x].find('분 전')]
-            writetime = datetime.today() - timedelta(minutes=int(timenumber))
-        elif '시간 전' in time[x]:
-            # 시간일 경우 작성 시간 확인
-            timenumber = time[x][0:time[x].find('시간 전')]
-            writetime = datetime.today() - timedelta(hours=int(timenumber))
-        elif '일 전' in time[x]:
-            # 일일 경우 작성 시간 확인
-            timenumber = time[x][0:time[x].find('일 전')]
-            writetime = datetime.today() - timedelta(days=int(timenumber))
-        elif '주 전' in time[x]:
-            # 주일 경우 작성 시간 확인
-            timenumber = time[x][0:time[x].find('주 전')]
-            writetime = datetime.today() - timedelta(weeks=int(timenumber))
-        elif '개월 전' in time[x]:
-            # 개월일 경우 작성 시간 확인
-            timenumber = time[x][0:time[x].find('개월 전')]
-            delta = relativedelta(months=int(timenumber))
-            writetime = datetime.today() - delta
-        elif '년 전' in time:
-            # 년일 경우 작성 시간 확인
-            timenumber = time[x][0:time[x].find('년 전')]
-            delta = relativedelta(years=int(timenumber))
-            writetime = datetime.today() - delta
-
+        delta = ''          # 개월, 년 차이 초기화
+        fileContent = ""    # fileContent 초기화
         utubeKey = ""       # 유튜브 키값 초기화
         url = ""            # url 초기화
         iframe = ""         # iframe 초기화
-        # tempstr = ""        # 임시 저장 초기화
-        
+        tempstr = ""        # 임시 저장 초기화
+
+        if '분 전' in time[x]:
+            # 분일 경우 작성 시간 확인
+            # timenumber = time[x][0:time[x].find('분 전')]
+            timenumber = re.findall("\d+", time[x])
+            writetime = datetime.today() - timedelta(minutes=int(timenumber[0]))
+        elif '시간 전' in time[x]:
+            # 시간일 경우 작성 시간 확인
+            # timenumber = time[x][0:time[x].find('시간 전')]
+            timenumber = re.findall("\d+", time[x])
+            writetime = datetime.today() - timedelta(hours=int(timenumber[0]))
+        elif '일 전' in time[x]:
+            # 일일 경우 작성 시간 확인
+            # timenumber = time[x][0:time[x].find('일 전')]
+            timenumber = re.findall("\d+", time[x])
+            writetime = datetime.today() - timedelta(days=int(timenumber[0]))
+        elif '주 전' in time[x]:
+            # 주일 경우 작성 시간 확인
+            # timenumber = time[x][0:time[x].find('주 전')]
+            timenumber = re.findall("\d+", time[x])
+            writetime = datetime.today() - timedelta(weeks=int(timenumber[0]))
+        elif '개월 전' in time[x]:
+            # 개월일 경우 작성 시간 확인
+            # timenumber = time[x][0:time[x].find('개월 전')]
+            timenumber = re.findall("\d+", time[x])
+            delta = relativedelta(months=int(timenumber[0]))
+            writetime = datetime.today() - delta
+        elif '년 전' in time:
+            # 년일 경우 작성 시간 확인
+            # timenumber = time[x][0:time[x].find('년 전')]
+            timenumber = re.findall("\d+", time[x])
+            delta = relativedelta(years=int(timenumber[0]))
+            writetime = datetime.today() - delta
+
+        # 유튜브 Video ID 추출
         utubeKey = firsturl[x][9 : 9 + 11]
+        # 유튜브 URL 만들기
         url = 'https://www.youtube.com/watch?v=' + str(utubeKey)
+        # iframe 태그 생성
         iframe = yt.video(url, width=width, height=height)
 
         if(writetime > todate):
-            print("작성 안 하고, 다음 URL 조회")
+            print("작성 안 하고, 다음 URL 조회 (당일 7시 이후)")
             print(title[x])
             print(url)
             print(writetime)
             pass
         elif writetime <= fromdate:
-            print("작성 대상 아님 - 더이상 URL 조회하지 않음")
+            print("작성 안 하고, 다음 URL 조회 (전일 7시 미만)")
             print(title[x])
             print(url)
             print(writetime)            
         else:
-            print("작성 대상 맞음")
+            print("작성 대상 맞음 (전일 7시 ~ 당일 6시 59분 59초)")
             print(title[x])
             print(url)
             print(writetime)
 
-            # 태그들은 임시 저장
+            # 태그들은 임시 변수에 저장
             tempstr = "<p>" + title[x] + "</p>" # 게시글 제목 앞에 <p> 추가, 제목 뒤에 </p> 추가. 2021.01.03 추가
             tempstr += "\n"
             tempstr += '<p><a target=_blank href="' + url + '">' + url + '</a></p>'
