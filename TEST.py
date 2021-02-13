3fb3abdd7a0df0018ae7db3900af90f0d9097287
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-13 23:36:32 +0900

    텐서플로우GUI 테스트

--- TEST.py
+++ TEST.py
@@ -1,7 +1,9 @@
-import re
+# import re
 
-temp = '<span class="style-scope ytd-grid-video-renderer">스트리밍 시간: 3주 전</span>'
+# temp = '<span class="style-scope ytd-grid-video-renderer">스트리밍 시간: 3주 전</span>'
 
-number = re.findall("\d+", temp)
+# number = re.findall("\d+", temp)
 
-print(number[0])
+# print(number[0])
+
+import tensorflow as tf
