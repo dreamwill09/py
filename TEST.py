8b1a10d7f711c34760970ede7a2d63f218d9047c
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-14 20:12:17 +0900

    폴더 정리

--- TEST.py
+++ TEST.py
@@ -1,9 +1,9 @@
-# import re
-
-# temp = '<span class="style-scope ytd-grid-video-renderer">스트리밍 시간: 3주 전</span>'
-
-# number = re.findall("\d+", temp)
-
-# print(number[0])
-
+""" 내용에서 숫자만 추출하는 방법
+import re
+temp = '<span class="style-scope ytd-grid-video-renderer">스트리밍 시간: 3주 전</span>'
+number = re.findall("\d+", temp)
+print(number[0])
+"""
+# 텐서플로우 정상 동작 테스트
+# Successfully opened dynamic library cudart64_110.dll 나오면 정상
 import tensorflow as tf
--- youtube_pafy.py
+++
@@ -1,6 +0,0 @@
-import pafy
-# myvid = pafy.new("https://www.youtube.com/watch?v=B6Yg6bwllM4")
-
-myvid = pafy.new("dQw4w9WgXcQ")
-
-print(myvid.published)
