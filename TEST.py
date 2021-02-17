89f377380945e7441efd0a25f5cce2426f6caa8b
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-17 21:29:52 +0900

    빈도 테스트

--- TEST.py
+++ TEST.py
@@ -6,4 +6,23 @@ print(number[0])
 """
 # 텐서플로우 정상 동작 테스트
 # Successfully opened dynamic library cudart64_110.dll 나오면 정상
-import tensorflow as tf
+# import tensorflow as tf
+
+from collections import Counter
+
+
+def sort_by_frequency(n):
+    # n을 카운터
+    most_n = Counter(n)
+    
+    most_n_list = sorted((count, -num) for num, count in most_n.items())
+    result = []
+
+    for count, num in most_n_list:
+        for i in range(count):
+            result.append(-num)
+
+    return result
+
+
+print(sort_by_frequency([3, 8, 8, 3, 2, 8, 1, 2, 4, 56]))
