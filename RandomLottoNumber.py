31a19d54faefa3217141a0d599bcbc048aa335e0
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-17 21:30:11 +0900

    로또 번호 생성기 개발 완료

--- RandomLottoNumber.py
+++ RandomLottoNumber.py
@@ -3,20 +3,19 @@ print('Lotto')
 # random을 import 한다.
 import random
 
+from collections import Counter
+
 # 횟수를 입력 받음
-count = int(input("로또 번호를 몇개 생성할까요?> "))
+count = int(input("로또 번호를 몇 개 생성할까요?> "))
 
 sumlotto = []
 no = []
+a = []
+b = 0
 
 for m in range(count):
 
-      lotto = []      
-
-      # 1 ~ 45 중에서 랜덤 생성
-      a=random.choices(range(1,46))
-
-      b=a[0]
+      lotto = []
 
       for i in range(6):
 
@@ -34,12 +33,11 @@ for m in range(count):
       # 생성된 로또 번호를 누적
       sumlotto += lotto
 
+# 원하는 만큼 생성된 로또 번호를 다 보여줍니다. 너무 많을 경우 파일로 저장하는 기능 만들기?
 print(sumlotto)
 
-# 1번 ~ 45번까지 빈도를 count
-for c in range(1,45+1):
-      no.append(sumlotto.count(c))
+# 리스트의 빈도를 구해주는 counter
+frequency = Counter(sumlotto)
 
-# 1번 ~ 45번까지 빈도를 print
-for d in range(0,44+1):
-      print(str(d+1) + " : " + str(no[d]))
+# 로또 번호별 빈도를 보여줍니다
+print(frequency)
