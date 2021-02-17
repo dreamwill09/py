4c285ce4f4df16f91fc9da58335e3760ffe951a6
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-17 19:06:09 +0900

    개발 중

--- RandomLottoNumber.py
+++ RandomLottoNumber.py
@@ -1,9 +1,9 @@
-# This is Lotto numbering program made by Suh, Changsoo.
-
 print('Lotto')
 
+# random을 import 한다.
 import random
 
+# 횟수를 입력 받음
 count = int(input("로또 번호를 몇개 생성할까요?> "))
 
 sumlotto = []
@@ -13,6 +13,7 @@ for m in range(count):
 
       lotto = []      
 
+      # 1 ~ 45 중에서 랜덤 생성
       a=random.choices(range(1,46))
 
       b=a[0]
@@ -27,15 +28,18 @@ for m in range(count):
 
             lotto.append(b)
 
+      # 로또 번호를 sort
       lotto.sort()
 
-      # print(str(m+1) + " : " + str(lotto))
-
+      # 생성된 로또 번호를 누적
       sumlotto += lotto
 
 print(sumlotto)
 
-# for c in range(1,45+1):
-#       # print(str(c))
-#       no1 = sumlotto.count(1)
-#       print(str(c) + " : " + str(no[c]))
+# 1번 ~ 45번까지 빈도를 count
+for c in range(1,45+1):
+      no.append(sumlotto.count(c))
+
+# 1번 ~ 45번까지 빈도를 print
+for d in range(0,44+1):
+      print(str(d+1) + " : " + str(no[d]))
